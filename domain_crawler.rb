7b5386b166f610ce9aa4925888494632cbc598c5
Michael Appell <mikeappell@gmail.com>
2016-06-30 00:15:58 -0400

    Keeping better tabs on which paths have already been traversed

--- domain_crawler.rb
+++ domain_crawler.rb
@@ -4,36 +4,39 @@ require "pry"
 
 class DomainCrawler
   attr_accessor :root_domain
+  attr_reader :traversed_paths, :elements
 
   def initialize(root_domain)
     @root_domain = root_domain
+    @traversed_paths = []
+    @elements = []
     # Ensure that root_domain is a proper http address
   end
 
   def inspect(current_path = nil, depth = 0)
     current_path ||= root_domain
-    @elements = []
+    traversed_paths << current_path
 
     begin
-      # binding.pry
-      html = RestClient.get(root_domain) # potential for errors/exceptions here?
+      html = RestClient.get(current_path)
 
       Nokogiri::HTML(html).traverse do |node|
         node = node.to_s.match(/(https?:\/\/.+?)\"/)
         if node  
           node = node_to_string(node, depth)
-          @elements << node unless @elements.last == node  # Gives better result then just doing .uniq, as you want children to be able to display copies of the same link
-          binding.pry if node.include?(root_domain)
-          inspect(node.strip, depth + 1) if node.include?(root_domain) && node != root_domain
+          elements << node unless elements.last == node  # Gives better result then just doing .uniq, as you want children to be able to display copies of the same link
+          # binding.pry if node.include?(root_domain)
+          inspect(node.strip, depth + 1) if node_traversible?(node.strip)
         end
       end
     rescue
+      puts "Not a traversible URL"
       # Not a traversible URL
     end
 
     unless depth != 0
       output_file = File.open("sitemap.txt", "w")
-      @elements.each { |element| output_file.write(element) }
+      elements.each { |element| output_file.write(element) }
       output_file.close
     end
   end
@@ -42,14 +45,13 @@ class DomainCrawler
 
   def node_to_string(node, depth)
     output_element = node[0].gsub("\"", "") # Regex is still catching the trailing "
-    # binding.pry
     depth.times { output_element = "\t\t" + output_element }
     output_element + "\n"
   end
 
-  # def is_element_traversible(node, current_path, depth)
-  #   node.include?(current_path)
-  # end
+  def node_traversible?(node)
+    node.include?(root_domain) && !traversed_paths.include?(node) && !node.end_with?(".php", ".js", ".xml")
+  end
 
 end
 
