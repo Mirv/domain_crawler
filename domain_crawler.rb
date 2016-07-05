242c112b3d7d22e3aef3692116be38710f84e882
Michael Appell <mikeappell@gmail.com>
2016-07-05 15:38:40 -0400

    Added parent to each item's listing

--- domain_crawler.rb
+++ domain_crawler.rb
@@ -1,13 +1,14 @@
 require "nokogiri"
 require "rest-client"
+require "pry"
 
 class DomainCrawler
-  attr_accessor :root_domain
-  attr_reader :traversed_paths, :traversible_paths, :elements
+  attr_accessor :root_domain, :current_path
+  attr_reader :traversed_paths, :traversible_paths, :sitemap_elements
 
   def initialize(root_domain)
     @root_domain = root_domain
-    @traversed_paths, @traversible_paths, @elements = [], [], []
+    @traversed_paths, @traversible_paths, @sitemap_elements = [], [], []
     @traversible_paths << root_domain
     # Ensure that root_domain is a proper http address?
   end
@@ -16,7 +17,7 @@ class DomainCrawler
     while !traversible_paths.empty?
       # Catch errors/exceptions from malformed URLs being sent into RestClient
       begin 
-        current_path = traversible_paths.first
+        self.current_path = traversible_paths.first
         traversed_paths << current_path
         site_html = RestClient.get(current_path)
 
@@ -30,17 +31,17 @@ class DomainCrawler
       traversible_paths.shift
     end
 
-    if elements.empty?
+    if sitemap_elements.empty?
       puts "Please enter a valid domain to crawl. This one returned no elements to traverse or record."
     else
-      output_sitemap(elements.uniq.sort)
+      output_sitemap(sitemap_elements.uniq.sort)
     end
   end
 
   private
 
   def node_traversible?(node)
-    node.start_with?(root_domain) &&
+    (node.start_with?(root_domain) || node.start_with?(root_domain.gsub("www.", ""))) &&
     !traversed_paths.include?(node) &&
     !traversible_paths.include?(node) &&
     !node.end_with?(".php", ".js", ".xml")
@@ -52,18 +53,16 @@ class DomainCrawler
 
   def examine_node(node)
     node = node[0].gsub("\"", "").strip
-    elements << node
-    puts "Node: #{node}" if node_traversible?(node)
-    # I left this in as a bit of a sanity check for the user, as the crawl can take quite a long time.
-    # The intermittent console feedback lets them know something is still happening.
-    traversible_paths << node if node_traversible?(node)
+    sitemap_elements << [current_path, node]
+    if node_traversible?(node)
+      puts "Current Path: #{current_path}\tNode: #{node}"
+      traversible_paths << node
+    end
   end
 
   def output_sitemap(sorted_elements)
     output_file = File.open("sitemap.txt", "w")
-    sorted_elements.each { |element| output_file.write(element + "\n") }
+    sorted_elements.each { |element| output_file.write("Parent: #{element[0]}\tElement: #{element[1]}\n") }
     output_file.close
   end
 end
-
-# DomainCrawler.new("https://figmentums.wordpress.com").inspect
